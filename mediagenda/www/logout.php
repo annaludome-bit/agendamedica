37a8e6c8aefb92added1245179ca75558a4a8a47
annaludome-bit <anna.lu.dome@gmail.com>
2026-06-06 19:19:52 -0300

    Delete mediagenda directory

--- mediagenda/www/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    if(!isset($_SESSION)){
-        session_start();
-    }
-    //destruir a sessão:
-    session_destroy();
-    //redirecionar para a página de login:
-    header("location:login.php");
-?>
